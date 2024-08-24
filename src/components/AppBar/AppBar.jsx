bcc37f95bb2420789b7e9e73005e7893cafe8827
Vladyslav Dushko <jafter@ukr.net>
2024-08-24 12:58:21 +0300

    navigation

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,6 +1,6 @@
 import { useSelector } from 'react-redux';
 import AuthNav from '../AuthNav/AuthNav';
-import Navigation from '../Navbar/Navigation';
+import Navigation from '../Navigation/Navigation';
 import UserMenu from '../UserMenu/UserMenu';
 import styles from './AppBar.module.css';
 import { selectIsLoggedIn } from '../../redux/auth/selectors';
